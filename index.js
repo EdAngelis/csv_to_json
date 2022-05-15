0b3d8d66b117b176ff5ba7727707152a0b2c45e2
Ed <ed4ngelis@gmail.com>
2022-05-15 16:38:06 -0300

    removing flags

--- index.js
+++ index.js
@@ -2,4 +2,28 @@ const fs = require("fs");
 
 csv = fs.readFileSync("input.csv");
 
-const csvArray = csv.toString().split("\n");
+const removeFlags = (str) => {
+  let inSideflag = false;
+  newStr = "";
+  for (let char of str) {
+    if (char === '"') {
+      inSideflag = !inSideflag;
+    }
+    if (char === "," && inSideflag) {
+      char = "/";
+    }
+    if (char !== '"') {
+      newStr += char;
+    }
+  }
+  return newStr;
+};
+
+const normalize = (str) => {
+  const noFlags = removeFlags(str);
+
+  return noFlags;
+};
+const csvString = csv.toString();
+
+const csvNormiled = normalize(csvString);
