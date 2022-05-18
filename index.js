9051f015a0a966d07eb99f00f47985141627c461
Ed <ed4ngelis@gmail.com>
2022-05-18 12:49:26 -0300

    factored

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ csv = fs.readFileSync("input2.csv");
 
 // METHODS BEGIN ##############################################################
 
-const removeFlags = (str) => {
+const removeQuotas = (str) => {
   let inSideflag = false;
   newStr = "";
+
   for (let char of str) {
     if (char === '"') {
       inSideflag = !inSideflag;
@@ -20,14 +21,11 @@ const removeFlags = (str) => {
   return newStr;
 };
 
-const splitElementsInMatrixBy = (matrix, char) => {
-  const headers = matrix[0];
-  for (let i = 1; i < matrix.length; i++) {
-    for (j in headers) {
-      matrix[i][j] = matrix[i][j].split(char);
-    }
+const sortGroups = (data) => {
+  for (i in data) {
+    data[i]["groups"].sort();
   }
-  return matrix;
+  return data;
 };
 
 const trimAllElement = (data) => {
@@ -41,12 +39,24 @@ const trimAllElement = (data) => {
   return data;
 };
 
+const splitElementsInMatrixBy = (matrix, char) => {
+  const headers = matrix[0];
+
+  for (let i = 1; i < matrix.length; i++) {
+    for (j in headers) {
+      matrix[i][j] = matrix[i][j].split(char);
+    }
+  }
+  return matrix;
+};
+
 const joinRowsByEid = (matrix) => {
-  const data = trimAllElement(matrix);
+  const data = trimAllElement(matrix); // remove spaces before and after a string from all elements
   const eidIndex = data[0].indexOf("eid");
   let newArray = [];
   let duplicate = false;
-  const headers = data[0];
+  const columns = data[0];
+
   for (i in data) {
     if (i <= 1) {
       newArray.push(data[i]);
@@ -54,13 +64,16 @@ const joinRowsByEid = (matrix) => {
     }
 
     for (j in newArray) {
-      if (newArray[j][eidIndex][0] === data[i][eidIndex][0]) {
+      const eidNewArray = newArray[j][eidIndex][0];
+      const eidData = data[i][eidIndex][0];
+      if (eidNewArray === eidData) {
         duplicate = true;
-        for (y in headers) {
-          const rowCell = data[i][y];
-          for (z in rowCell) {
-            if (!newArray[j][y].includes(rowCell[z])) {
-              newArray[j][y].push(rowCell[z]);
+        for (c in columns) {
+          const cell = data[i][c];
+          for (r in cell) {
+            const elementInArray = newArray[j][c].includes(cell[r]);
+            if (!elementInArray) {
+              newArray[j][c].push(cell[r]);
             }
           }
         }
@@ -75,48 +88,81 @@ const joinRowsByEid = (matrix) => {
   return newArray;
 };
 
-const sortGroups = (data) => {
-  for (i in data) {
-    data[i]["groups"].sort();
+const emailPhoneValidate = (data) => {
+  const headers = data[0].map((header) => header.split(" "));
+
+  for (let i = 1; i < data.length; i++) {
+    for (j in headers) {
+      const header = headers[j][0];
+      if (header === "email" || header === "phone") {
+        for (k in data[i][j]) {
+          switch (header) {
+            case "email":
+              // the expression below generate a array with the email in position 0 if there is a valid email
+              const email = data[i][j][k].match(/\S+@[^\s.]+\.[^.\s]+/);
+              if (email) {
+                data[i][j][k] = email[0];
+              } else {
+                data[i][j][k] = "";
+              }
+              break;
+            case "phone":
+              const phone = data[i][j][k].replace(/\D/g, ""); // thake just the numbers from the phone
+              if (phone.length === 11) {
+                data[i][j][k] = "55" + phone;
+              } else {
+                data[i][j][k] = "";
+              }
+              break;
+          }
+        }
+      }
+    }
   }
   return data;
 };
 
 const matrixToObj = (headers, data) => {
   let arrayOfObj = [];
+
   for (i in data) {
     let obj = {};
     obj["groups"] = [];
     obj["addresses"] = [];
+
     for (j in headers) {
-      const rowCell = data[i][j];
+      const cell = data[i][j];
       const header = headers[j][0];
+
       switch (header) {
         case "eid":
         case "fullname":
-          obj[header] = rowCell[0];
+          obj[header] = cell[0];
           break;
+
         case "group":
-          const groups = rowCell;
+          const groups = cell;
           for (g in groups) {
-            const group = rowCell[g];
+            const group = cell[g];
             if (group !== "") {
               obj["groups"].push(group);
             }
           }
           break;
+
         case "see_all":
         case "invisible":
-          if (rowCell[0] === "1" || rowCell[0] === "yes") {
+          if (cell[0] === "1" || cell[0] === "yes") {
             obj[header] = true;
           } else {
             obj[header] = false;
           }
           break;
+
         case "phone":
         case "email":
-          for (k in rowCell) {
-            const address = rowCell[k];
+          for (k in cell) {
+            const address = cell[k];
             if (address) {
               let adress = {
                 type: headers[j][0],
@@ -135,53 +181,22 @@ const matrixToObj = (headers, data) => {
   return sortGroups(arrayOfObj);
 };
 
-const emailPhoneNormalize = (data) => {
-  const headers = data[0].map((header) => header.split(" "));
-
-  for (let i = 1; i < data.length; i++) {
-    for (j in headers) {
-      const header = headers[j][0];
-      if (header === "email" || header === "phone") {
-        for (k in data[i][j]) {
-          switch (header) {
-            case "email":
-              // the expression below generate a array with the email in position 0
-              const email = data[i][j][k].match(/\S+@[^\s.]+\.[^.\s]+/);
-              if (email) {
-                data[i][j][k] = email[0];
-              } else {
-                data[i][j][k] = "";
-              }
-              break;
-            case "phone":
-              const phone = data[i][j][k].replace(/\D/g, "");
-              if (phone.length === 11) {
-                data[i][j][k] = "55" + phone;
-              } else {
-                data[i][j][k] = "";
-              }
-              break;
-          }
-        }
-      }
-    }
-  }
-  return data;
-};
-
 // METHODS END ################################################################
 
 let csvString = csv.toString();
 
-csvString = removeFlags(csvString);
+// in csv two elements can come as one  when inside quotas and separeted by commas
+// the function remove the quotas and put / rather than commas to normalize all data
+csvString = removeQuotas(csvString);
 
 const csvArray = csvString.split("\n");
 
 let csvMatrix = csvArray.map((row) => row.split(","));
 
+// separete elements when in same cell
 csvMatrix = splitElementsInMatrixBy(csvMatrix, "/");
 
-csvMatrix = emailPhoneNormalize(csvMatrix);
+csvMatrix = emailPhoneValidate(csvMatrix);
 
 csvMatrix = joinRowsByEid(csvMatrix);
 
